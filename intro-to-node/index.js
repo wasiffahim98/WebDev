0b33628299b9f921171bd3350e46cb08d885135d
Wasif Fahim <wasif.fahim@gmail.com>
2022-09-05 12:32:23 -0700

    added in nodeJS packages

---
+++ intro-to-node/index.js
@@ -0,0 +1,7 @@
+var superheroes = require("superheroes");
+var mySuperheroName = superheroes.random();
+console.log(mySuperheroName);
+
+var supervillians = require("supervillains");
+var mySuperVillianName = supervillians.random();
+console.log(mySuperVillianName);
