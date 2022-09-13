a9fd389df34d82a0daf706d63af19d4a05cdb647
Wasif Fahim <wasif.fahim@gmail.com>
2022-09-13 14:59:02 -0700

    added in level 1 security

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,1 +1,74 @@
 //jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/", function(request, response){
+    response.render("home");
+});
+
+app.get("/login", function(request, response){
+    response.render("login");
+});
+
+app.get("/register", function(request, response){
+    response.render("register");
+});
+
+app.post("/register", function(request, response){
+    const newUser = new User({
+        email: request.body.username,
+        password: request.body.password
+    });
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            response.render("secrets");
+        }
+    });
+});
+
+app.post("/login", function(request, response){
+    const username = request.body.username;
+    const password = request.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if (err){
+            console.log(err);
+        }
+        else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    response.render("secrets");
+                }
+            }
+        }
+    });
+});
+
+
+app.listen(3000, function(){
+    console.log("server is running on port 3000");
+})
